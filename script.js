88f993319174cfc69debd484c30c333760dc53cd
Christian Ruiz <lemmyruiz@Christians-MacBook-Pro.local>
2022-01-26 12:45:14 -0800

    added vars and more javascript

--- script.js
+++ script.js
@@ -12,6 +12,56 @@ $('.table').find('.eventTextBox').each(function (val) {
     console.log($(this).data('time'));
 
     if (hours === $(this).data('time')) {
+        $(this).css('background-color', 'green');
+    }
+    else if (hours > $(this).data('time')) {
         $(this).css('background-color', 'red');
     }
+    else {
+        $(this).css('background-color', 'yellow');
+    }
 })
+
+$('.save').on("click", function () {
+    
+    var note = $(this).parent().parent().children().find('input').val();
+    // var hour = $(this).siblings("input").attr("data-time")
+    console.log(note);
+    var myStorage = window.localStorage;
+    
+});
+
+
+
+
+
+//localstorage.setitem(hour, note)
+
+
+
+
+
+
+
+//// onclick function  for the button {
+    //
+//  read the input that is the sibling of the button and read the attribute data-time
+// store in the localsotrate the info with the hour and the text
+// key as the hour and the value as the input info
+
+// var note= $(this).siblings("input").val()
+// var hour  = $(this).siblings("input").attr("data-time")
+// console.log("--->", hour, note)
+// localstorage.setitem(hour,note)
+
+
+///}
+
+
+
+
+// reload  
+
+/// you need to be able to show the info that was saved in the localstorage
+
+//look   / localstorage.getItem
