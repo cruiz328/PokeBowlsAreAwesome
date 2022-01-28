db81418c9316c99d8b2c3166ce15b39720957c72
Christian Ruiz <lemmyruiz@Christians-MacBook-Pro.local>
2022-01-28 02:51:21 -0800

    trying to add local storage

--- script.js
+++ script.js
@@ -25,43 +25,7 @@ $('.table').find('.eventTextBox').each(function (val) {
 $('.save').on("click", function () {
     
     var note = $(this).parent().parent().children().find('input').val();
-    // var hour = $(this).siblings("input").attr("data-time")
-    console.log(note);
+    var hour = $(this).siblings("input").attr("data-time")
+   // console.log(note);
     var myStorage = window.localStorage;
-    
 });
-
-
-
-
-
-//localstorage.setitem(hour, note)
-
-
-
-
-
-
-
-//// onclick function  for the button {
-    //
-//  read the input that is the sibling of the button and read the attribute data-time
-// store in the localsotrate the info with the hour and the text
-// key as the hour and the value as the input info
-
-// var note= $(this).siblings("input").val()
-// var hour  = $(this).siblings("input").attr("data-time")
-// console.log("--->", hour, note)
-// localstorage.setitem(hour,note)
-
-
-///}
-
-
-
-
-// reload  
-
-/// you need to be able to show the info that was saved in the localstorage
-
-//look   / localstorage.getItem
